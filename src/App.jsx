8286a84b4ea2d16f9a9656096e02ee88c2e92aeb
Marc Pomar <marc@corecode.school>
2022-07-28 19:15:20 +0200

    fix: landing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import { useState } from "react";
+import { Hero } from "./components/Hero";
+import { Section } from "./components/Section";
+import { Toolbar } from "./components/Toolbar";
+import { tesla_images } from "./lib/tesla_images";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="text-center py-20">
-      <img
-        src="https://www.corecode.school/branding/logo_app.png"
-        width="50"
-        className="mx-auto my-4"
-      />
-      <h1 className="text-orange-500 text-2xl font-bold text-center">
-        CORE Code School
-      </h1>
+    <div>
+      <Section bg={tesla_images.model_3}>
+        <Hero title="Model 3" subtitle="Order Online for Touchless Delivery" />
+        <Toolbar />
+      </Section>
+      <Section bg={tesla_images.model_y}>
+        <Hero title="Model Y" subtitle="Order Online for Touchless Delivery" />
+        <Toolbar />
+      </Section>
+      <Section bg={tesla_images.model_x}>
+        <Hero title="Model X" subtitle="Order Online for Touchless Delivery" />
+        <Toolbar />
+      </Section>
     </div>
   );
 }
